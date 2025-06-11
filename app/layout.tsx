0356d252ccceebc4b1720337a1d4fc40e0d489e2
aniketnedunuri1 <aniketn16@gmail.com>
2025-06-10 22:06:37 -0400

    pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,22 @@ export default function RootLayout({
         <Header />
         <main className="pt-[65px] pb-[60px]">{children}</main>
         <Footer />
+        
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1388049735679585');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        
         <Script
           src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=VicFLw"
           strategy="afterInteractive"
