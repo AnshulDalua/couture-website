4c6bf4fe17657b1490eda8b0655b9dd9929ce8c9
aniketnedunuri1 <aniketn16@gmail.com>
2026-02-02 10:53:02 -0500

    Remove sororotiy pics on our designs

--- app/our-designs/page.tsx
+++ app/our-designs/page.tsx
@@ -16,108 +16,108 @@ const designsData: {
 } = {
   title: "Our Designs",
   designs: [
-    {
-      image: "/designs/DGLightBlue.webp",
-      caption: "DELTA GAMMA LIGHT BLUE DESIGN",
-      printStyle: "SCREEN PRINT",
-      organization: "DELTA GAMMA"
-    },
-    {
-      image: "/designs/A Chi O Tank.webp",
-      caption: "AXO `RABBIT` TANK CONCEPT",
-      printStyle: "EMBRODIERY",
-      organization: "ALPHA CHI OMEGA"
-    },
+    // {
+    //   image: "/designs/DGLightBlue.webp",
+    //   caption: "DELTA GAMMA LIGHT BLUE DESIGN",
+    //   printStyle: "SCREEN PRINT",
+    //   organization: "DELTA GAMMA"
+    // },
+    // {
+    //   image: "/designs/A Chi O Tank.webp",
+    //   caption: "AXO `RABBIT` TANK CONCEPT",
+    //   printStyle: "EMBRODIERY",
+    //   organization: "ALPHA CHI OMEGA"
+    // },
     // {
     //   image: "/designs/MSU.webp",
     //   caption: "MICHIGAN STATE UNIVERSITY DESIGN",
     //   printStyle: "SCREEN PRINT",
     //   organization: "MSU"
     // },
-    {
-      image: "/designs/KKGHoodie (1).webp",
-      caption: "KKG `KEY` HOODIE CONCEPT",
-      printStyle: "PUFF PRINT",
-      organization: "KAPPA KAPPA GAMMA"
-    },
-    {
-      image: "/designs/TriDelta.webp",
-      caption: "TRI DELTA DESIGN",
-      printStyle: "SCREEN PRINT",
-      organization: "DELTA DELTA DELTA"
-    },
-    {
-      image: "/designs/ThetaTank.webp",
-      caption: "THETA `STAR` TANK CONCEPT",
-      printStyle: "SCREEN PRINT",
-      organization: "KAPPA ALPHA THETA"
-    },
-    {
-      image: "/designs/KKGWF.webp",
-      caption: "KAPPA KAPPA GAMMA WF DESIGN",
-      printStyle: "SCREEN PRINT",
-      organization: "KAPPA KAPPA GAMMA"
-    },
-    {
-      image: "/designs/AphiHoodie.webp",
-      caption: "APHI `FLOWER` HOODIE CONCEPT",
-      printStyle: "SCREEN PRINT",
-      organization: "ALPHA PHI"
-    },
-    {
-      image: "/designs/DGRoyalBlue.webp",
-      caption: "DELTA GAMMA ROYAL BLUE DESIGN",
-      printStyle: "SCREEN PRINT",
-      organization: "DELTA GAMMA"
-    },
-    {
-      image: "/designs/AphiHoodie. (1).webp",
-      caption: "APHI EMBROIRDERED TEXT DESIGN CONCEPT",
-      printStyle: "EMBROIDERY",
-      organization: "ALPHA PHI"
-    },
+    // {
+    //   image: "/designs/KKGHoodie (1).webp",
+    //   caption: "KKG `KEY` HOODIE CONCEPT",
+    //   printStyle: "PUFF PRINT",
+    //   organization: "KAPPA KAPPA GAMMA"
+    // },
+    // {
+    //   image: "/designs/TriDelta.webp",
+    //   caption: "TRI DELTA DESIGN",
+    //   printStyle: "SCREEN PRINT",
+    //   organization: "DELTA DELTA DELTA"
+    // },
+    // {
+    //   image: "/designs/ThetaTank.webp",
+    //   caption: "THETA `STAR` TANK CONCEPT",
+    //   printStyle: "SCREEN PRINT",
+    //   organization: "KAPPA ALPHA THETA"
+    // },
+    // {
+    //   image: "/designs/KKGWF.webp",
+    //   caption: "KAPPA KAPPA GAMMA WF DESIGN",
+    //   printStyle: "SCREEN PRINT",
+    //   organization: "KAPPA KAPPA GAMMA"
+    // },
+    // {
+    //   image: "/designs/AphiHoodie.webp",
+    //   caption: "APHI `FLOWER` HOODIE CONCEPT",
+    //   printStyle: "SCREEN PRINT",
+    //   organization: "ALPHA PHI"
+    // },
+    // {
+    //   image: "/designs/DGRoyalBlue.webp",
+    //   caption: "DELTA GAMMA ROYAL BLUE DESIGN",
+    //   printStyle: "SCREEN PRINT",
+    //   organization: "DELTA GAMMA"
+    // },
+    // {
+    //   image: "/designs/AphiHoodie. (1).webp",
+    //   caption: "APHI EMBROIRDERED TEXT DESIGN CONCEPT",
+    //   printStyle: "EMBROIDERY",
+    //   organization: "ALPHA PHI"
+    // },
     // {
     //   image: "/designs/KDShirt.webp",
     //   caption: "KAPPA DELTA SHIRT DESIGN",
     //   printStyle: "SCREEN PRINT",
     //   organization: "KAPPA DELTA"
     // },
-    {
-      image: "/designs/AphiPants (1).webp",
-      caption: "APHI `STAR` SWEAT CONCEPT",
-      printStyle: "EMBROIDERY",
-      organization: "ALPHA PHI"
-    },
+    // {
+    //   image: "/designs/AphiPants (1).webp",
+    //   caption: "APHI `STAR` SWEAT CONCEPT",
+    //   printStyle: "EMBROIDERY",
+    //   organization: "ALPHA PHI"
+    // },
     // {
     //   image: "/designs/PCTUmichSweater.webp",
     //   caption: "PCT U-MICH SWEATER DESIGN",
     //   printStyle: "EMBROIDERY",
     //   organization: "PCT"
     // },
-    {
-      image: "/designs/AphiHoodie (1)(2).webp",
-      caption: "ALPHA PHI HOODIE DESIGN",
-      printStyle: "SCREEN PRINT",
-      organization: "ALPHA PHI"
-    },
+    // {
+    //   image: "/designs/AphiHoodie (1)(2).webp",
+    //   caption: "ALPHA PHI HOODIE DESIGN",
+    //   printStyle: "SCREEN PRINT",
+    //   organization: "ALPHA PHI"
+    // },
     {
       image: "/designs/LBSAStar.webp",
       caption: "LBSA STAR DESIGN",
       printStyle: "SCREEN PRINT",
       organization: "LBSA"
     },
-    {
-      image: "/designs/AphiPants.webp",
-      caption: "ALPHA PHI PANTS DESIGN",
-      printStyle: "SCREEN PRINT",
-      organization: "ALPHA PHI"
-    },
-    {
-      image: "/designs/DZ Tank.webp",
-      caption: "DELTA ZETA TANK",
-      printStyle: "SCREEN PRINT",
-      organization: "DELTA ZETA"
-    },
+    // {
+    //   image: "/designs/AphiPants.webp",
+    //   caption: "ALPHA PHI PANTS DESIGN",
+    //   printStyle: "SCREEN PRINT",
+    //   organization: "ALPHA PHI"
+    // },
+    // {
+    //   image: "/designs/DZ Tank.webp",
+    //   caption: "DELTA ZETA TANK",
+    //   printStyle: "SCREEN PRINT",
+    //   organization: "DELTA ZETA"
+    // },
     {
       image: "/designs/TSAHoodieUpdated.webp",
       caption: "TSA HOODIE UPDATED DESIGN",
@@ -136,90 +136,90 @@ const designsData: {
     //   printStyle: "SCREEN PRINT",
     //   organization: "EVANS SCHOLARS"
     // },
-    {
-      image: "/designs/FishPants.webp",
-      caption: "FISH PANTS DESIGN",
-      printStyle: "EMBROIDERY",
-      organization: "KKG"
-    },
+    // {
+    //   image: "/designs/FishPants.webp",
+    //   caption: "FISH PANTS DESIGN",
+    //   printStyle: "EMBROIDERY",
+    //   organization: "KKG"
+    // },
     {
       image: "/designs/Girls Varsity tennis.webp",
       caption: "GIRLS VARSITY TENNIS",
       printStyle: "SCREEN PRINT",
       organization: "VARSITY TENNIS"
     },
-    {
-      image: "/designs/KKG Tank.webp",
-      caption: "KAPPA KAPPA GAMMA TANK",
-      printStyle: "SCREEN PRINT",
-      organization: "Kappa Kappa Gamma"
-    },
+    // {
+    //   image: "/designs/KKG Tank.webp",
+    //   caption: "KAPPA KAPPA GAMMA TANK",
+    //   printStyle: "SCREEN PRINT",
+    //   organization: "Kappa Kappa Gamma"
+    // },
     {
       image: "/designs/Accapella.webp",
       caption: "GIMBLE ACAPELLA GROUP DESIGN CONCEPT",
       printStyle: "EMBROIDERY",
       organization: "GIMBLE ACAPELLA GROUP"
     },
-    {
-      image: "/designs/KKGHoodie.webp",
-      caption: "KAPPA KAPPA GAMMA HOODIE",
-      printStyle: "PUFF PRINT",
-      organization: "KAPPA KAPPA GAMMA"
-    },
-    {
-      image: "/designs/KKGHoodie (2).webp",
-      caption: "KAPPA KAPPA GAMMA HOODIE V2",
-      printStyle: "EMBROIDERY",
-      organization: "KAPPA KAPPA GAMMA"
-    },
-    {
-      image: "/designs/KKGHoodie (3).webp",
-      caption: "KAPPA KAPPA GAMMA `FISH` HOODIE",
-      printStyle: "SCREEN PRINT",
-      organization: "KAPPA KAPPA GAMMA"
-    },
-    {
-      image: "/designs/KKGOLF back-35.webp",
-      caption: "KKG GOLF BACK DESIGN",
-      printStyle: "SCREEN PRINT",
-      organization: "KAPPA KAPPA GAMMA"
-    },
-    {
-      image: "/designs/KKGolf Front.webp",
-      caption: "KKG GOLF FRONT DESIGN",
-      printStyle: "SCREEN PRINT",
-      organization: "Kappa Kappa Gamma"
-    },
-    {
-      image: "/designs/KKGPants.webp",
-      caption: "KAPPA KAPPA GAMMA SWEATS",
-      printStyle: "PUFF PRINT",
-      organization: "KAPPA KAPPA GAMMA"
-    },
-    {
-      image: "/designs/KKGPants (1).webp",
-      caption: "KAPPA KAPPA GAMMA SWEATS",
-      printStyle: "PUFF PRINT",
-      organization: "KAPPA KAPPA GAMMA"
-    },
+    // {
+    //   image: "/designs/KKGHoodie.webp",
+    //   caption: "KAPPA KAPPA GAMMA HOODIE",
+    //   printStyle: "PUFF PRINT",
+    //   organization: "KAPPA KAPPA GAMMA"
+    // },
+    // {
+    //   image: "/designs/KKGHoodie (2).webp",
+    //   caption: "KAPPA KAPPA GAMMA HOODIE V2",
+    //   printStyle: "EMBROIDERY",
+    //   organization: "KAPPA KAPPA GAMMA"
+    // },
+    // {
+    //   image: "/designs/KKGHoodie (3).webp",
+    //   caption: "KAPPA KAPPA GAMMA `FISH` HOODIE",
+    //   printStyle: "SCREEN PRINT",
+    //   organization: "KAPPA KAPPA GAMMA"
+    // },
+    // {
+    //   image: "/designs/KKGOLF back-35.webp",
+    //   caption: "KKG GOLF BACK DESIGN",
+    //   printStyle: "SCREEN PRINT",
+    //   organization: "KAPPA KAPPA GAMMA"
+    // },
+    // {
+    //   image: "/designs/KKGolf Front.webp",
+    //   caption: "KKG GOLF FRONT DESIGN",
+    //   printStyle: "SCREEN PRINT",
+    //   organization: "Kappa Kappa Gamma"
+    // },
+    // {
+    //   image: "/designs/KKGPants.webp",
+    //   caption: "KAPPA KAPPA GAMMA SWEATS",
+    //   printStyle: "PUFF PRINT",
+    //   organization: "KAPPA KAPPA GAMMA"
+    // },
+    // {
+    //   image: "/designs/KKGPants (1).webp",
+    //   caption: "KAPPA KAPPA GAMMA SWEATS",
+    //   printStyle: "PUFF PRINT",
+    //   organization: "KAPPA KAPPA GAMMA"
+    // },
     {
       image: "/designs/NSHSSLA.webp",
       caption: "NSHSSLA DESIGN",
       printStyle: "SCREEN PRINT",
       organization: "NSHSSLA"
     },
-    {
-      image: "/designs/Pi Phi Tank.webp",
-      caption: "PI BETA PHI `ROCKSTAR` TANK",
-      printStyle: "SCREEN PRINT",
-      organization: "Pi BETA PHI"
-    },
-    {
-      image: "/designs/PiPhi.webp",
-      caption: "PI BETA PHI DESIGN",
-      printStyle: "SCREEN PRINT",
-      organization: "PI BETA PHI"
-    },
+    // {
+    //   image: "/designs/Pi Phi Tank.webp",
+    //   caption: "PI BETA PHI `ROCKSTAR` TANK",
+    //   printStyle: "SCREEN PRINT",
+    //   organization: "Pi BETA PHI"
+    // },
+    // {
+    //   image: "/designs/PiPhi.webp",
+    //   caption: "PI BETA PHI DESIGN",
+    //   printStyle: "SCREEN PRINT",
+    //   organization: "PI BETA PHI"
+    // },
     // {
     //   image: "/designs/Reach Consulting Group.webp",
     //   caption: "REACH CONSULTING GROUP `STREETWEAR` DESIGN",
@@ -232,42 +232,42 @@ const designsData: {
       printStyle: "SCREEN PRINT",
       organization: "REACH CONSULTING GROUP"
     },
-    {
-      image: "/designs/SeniorSetDG (1).webp",
-      caption: "DELTA GAMMA SENIOR SET",
-      printStyle: "EMBROIDERY",
-      organization: "DELTA GAMMA"
-    },
-    {
-      image: "/designs/SigChiHoodie.webp",
-      caption: "SIGMA CHI HOODIE",
-      printStyle: "EMBROIDERY",
-      organization: "SIGMA CHI"
-    },
-    {
-      image: "/designs/SigChiHoodie (1).webp",
-      caption: "SIGMA CHI HOODIE DESIGN CONCEPT",
-      printStyle: "PUFF PRINT",
-      organization: "SIGMA CHI"
-    },
-    {
-      image: "/designs/SigChiHoodie5.webp",
-      caption: "SIGMA CHI HOODIE DESIGN CONCEPT",
-      printStyle: "EMBROIDERY",
-      organization: "Sigma Chi"
-    },
-    {
-      image: "/designs/ThetaTankpng.webp",
-      caption: "THETA TANK AND BOY SHORTS",
-      printStyle: "SCREEN PRINT",
-      organization: "KAPPA ALPHA THETA"
-    },
-    {
-      image: "/designs/UMichSigChiHoodie.webp",
-      caption: "U-MICH SIGMA CHI HOODIE",
-      printStyle: "EMBROIDERY",
-      organization: "SIGMA CHI"
-    }
+    // {
+    //   image: "/designs/SeniorSetDG (1).webp",
+    //   caption: "DELTA GAMMA SENIOR SET",
+    //   printStyle: "EMBROIDERY",
+    //   organization: "DELTA GAMMA"
+    // },
+    // {
+    //   image: "/designs/SigChiHoodie.webp",
+    //   caption: "SIGMA CHI HOODIE",
+    //   printStyle: "EMBROIDERY",
+    //   organization: "SIGMA CHI"
+    // },
+    // {
+    //   image: "/designs/SigChiHoodie (1).webp",
+    //   caption: "SIGMA CHI HOODIE DESIGN CONCEPT",
+    //   printStyle: "PUFF PRINT",
+    //   organization: "SIGMA CHI"
+    // },
+    // {
+    //   image: "/designs/SigChiHoodie5.webp",
+    //   caption: "SIGMA CHI HOODIE DESIGN CONCEPT",
+    //   printStyle: "EMBROIDERY",
+    //   organization: "Sigma Chi"
+    // },
+    // {
+    //   image: "/designs/ThetaTankpng.webp",
+    //   caption: "THETA TANK AND BOY SHORTS",
+    //   printStyle: "SCREEN PRINT",
+    //   organization: "KAPPA ALPHA THETA"
+    // },
+    // {
+    //   image: "/designs/UMichSigChiHoodie.webp",
+    //   caption: "U-MICH SIGMA CHI HOODIE",
+    //   printStyle: "EMBROIDERY",
+    //   organization: "SIGMA CHI"
+    // }
   ]
 }
 
